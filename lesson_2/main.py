13f6e60575cb89e1dc518cff648877f9c97dd56e
nikit <nikitamarincenko5@gmail.com>
2024-09-22 12:51:16 +0300

    "Battle till death" code added

--- lesson_2/main.py
+++ lesson_2/main.py
@@ -1,12 +1,22 @@
 from character import Character
 
 player1 = Character('Nik', 100, 3, 0)
+player2 = Character('Alex', 100, 3, 0)
 
 print(f'Створено нового персонажа: {player1.name}')
 print(f'У {player1.name} {player1.health} здоров`я')
+print(f'Створено нового персонажа: {player2.name}')
+print(f'У {player2.name} {player2.health} здоров`я')
 
-incoming_damage = 20
-print(f'{player1.name} отримав урон у кількості {incoming_damage}')
-player1.health -= incoming_damage
-print(f'У {player1.name} {player1.health} здоров`я')
-player1.show_stats()
+while True:
+     print(f'{player1.name} атакував {player2.name}')
+     player2.health -= player1.damage + 10
+     print(f'У {player2.name} залишилося {player2.health}, після атаки {player1.name}'
+           f'{player2.name} атакував у відповідь!')
+     player1.health -= player2.damage + 10
+     if player2.health <= 0:
+         print(f'{player2.name} не переживе цю битву')
+         break
+     elif player1.health <= 0:
+         print(f'{player1.name} не переживе цю битву')
+         break
