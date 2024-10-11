48348e321ba645bce2ac8cd55d3f866aa6b0455f
nikit <nikitamarincenko5@gmail.com>
2024-10-11 16:07:32 +0300

    disney api added

--- lesson_4/main.py
+++ lesson_4/main.py
@@ -1,15 +1,43 @@
 import requests
+import random
 
-response = requests.get('https://whenisthenextmcufilm.com/api')
-if response.ok:
-    film = response.json()
-    print(film)
+page = random.randint(1, 7438)
+pageSize = 1
 
-    print('\n\nNEXT MCU FILM IS')
-    print(f"{film['title']} releases in {film['days_until']} days!")
-    print(film['overview'])
-    print(f"Release Date: {film['release_date']}")
-    print(f"Production Type: {film['type']}")
-    print(f"What`s afterwards? {film['following_production']['title']}")
-else:
-    print(f'{response.status_code}')
+def get_random_disney_character():
+    response = requests.get("https://api.disneyapi.dev/character?pageSize={pageSize}&page={page}")
+
+    if response.status_code == 200:
+        characters = response.json()["data"]
+
+        character = random.choice(characters)
+
+        print(f"Вам випав: {character['name']}")
+
+        films = character.get("films", [])
+        shorts = character.get("shortFilms", [])
+        tv_shows = character.get("tvShows", [])
+        video_games = character.get("videoGames", [])
+
+        print("Фільми:")
+        for film in films:
+            print(f"- {film} -")
+
+        print("\nКороткометражки:")
+        for short in shorts:
+            print(f"- {short} -")
+
+        print("\nСеріали:")
+        for tv_show in tv_shows:
+            print(f"- {tv_show} -")
+
+        print("\nІгри:")
+        for game in video_games:
+            print(f"- {game} -")
+    else:
+        print("Щось пішло не так...")
+        print(f'{response.status_code=}')
+
+
+if __name__ == "__main__":
+    get_random_disney_character()
